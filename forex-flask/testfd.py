df9771fab2c2d05fec71994d9ced6937bb3e23d2
nkarnik <n.karnik@yahoo.com>
2014-07-05 08:10:50 +0000

    removed forex-flask

--- forex-flask/testfd.py
+++
@@ -1,7 +0,0 @@
-from fluent import sender
-from fluent import event
-sender.setup('hdfs', host='localhost', port=24224)
-event.Event('testfd.forex', {
-    'pair': 'USDJPY',
-    'value': '120.32'
-})
